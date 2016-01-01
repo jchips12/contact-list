702d0920eb0fc7f412cb9213fc63ccfe5cc01736
JC Hipolito <jchips12@gmail.com>
2016-01-01 18:10:52 +0800

    added bower config

--- contact-api/src/main/resources/static/js/controller/HomeController.js
+++ contact-api/src/main/resources/static/js/controller/HomeController.js
@@ -4,18 +4,17 @@
 	
 	var HomeController = function($scope, $http) {
 		
-		$scope.person = {};
 		$http({
 			method : 'GET',
 			url : '/person/1'
 		}).then(function successCallback(response) {
 			$scope.status = response.status;
 			$scope.person = response.data;
+			$scope.person.personId = '1234567890123';
 		}, function errorCallback(response) {
 			$scope.status = response.status;
 			$scope.error = response.data;
 		});
-		$scope.person.personId = 1234567890;
 		
 	}
 	
